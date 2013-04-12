7dc3c323be01450e5d5b36b532312f3807c5905e
Frederico Andrade <faandrade@gmail.com>
2013-04-12 02:47:22 -0400

    fixed api

--- week9/data-final/server.js
+++ week9/data-final/server.js
@@ -5,38 +5,61 @@ var express = require('express'),
 
 var languages = require('./languages.json')
 
-// localhost:8888 introduces api
+// localhost:8080 introduces api
 
 app.get('/', function (req, res) {
-    res.send(
-        '<div style="position:fixed; top:0; left:0; width:100%; height:100%; background:#f1f1f1; text-align:center; font-family:georgia; color:#333; text-shadow:0px 1px 0px #fff">' +
-        '<h2 style="margin:25px 0px;">Languages of Spain</h2>' +
-        '<p style="margin:0px; font-size:14pt;">' +
-        'For data on Spain\'s eight native languages, <a style="color:#333;" href="http://localhost:8080/data">click here</a>.' +
-        '<br />' +
-        '</div>'
-        )
+    var returnString = 'Hi there, for raw data on spain, please use /languages.'
+    res.send(returnString)
 })
 
-// localhost:8888/data returns stringified data
-
-
+// localhost:8080/languages returns stringified data
 
 app.get('/languages', function (req, res){
-    var callback = req.query.callback
-    res.end(callback + '(' + JSON.stringify(languages) + ')')
+
+    var name = req.query.name,
+        nativeName = req.query.nativeName,
+        status = req.query.status,
+        population = req.query.populationL1,
+        callback = req.query.callback
+
+    var returnArray = []
+
+    languages.forEach(function(data){
+
+            var qualified = true
+
+            if (name && data.name !== name) {
+                qualified = false
+            }
+
+            if (nativeName && data.nativeName !== nativeName) {
+                qualified = false
+            }
+
+            if (status && data.status !== status) {
+                qualified = false
+            }
+
+            if (qualified) {
+                returnArray.push(data)
+            }
+
+        })
+
+
+    if (callback){
+        res.send(callback + '(' + JSON.stringify(returnArray) + ')')
+    } else {
+        res.send(returnArray)
+    }
+
+    //res.send(callback + '(' + JSON.stringify(languages) + ')')
 })
 
 // run node server.js in terminal
 
 app.listen(8080)
 
-// terminal will return this message if the server is running. go to localhost:8888 for a look at the data
+// terminal will return this message if the server is running. go to localhost:8080 for a look at the data
 
 console.log('Check out the languages of Spain!')
-
-
-
-
-
-//
